7b8f48242b2fa393d3ae83d6a5218b21ddfe2ee2
Dasthu <118829965+kuspilla@users.noreply.github.com>
2023-11-22 07:48:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+  const d1 = new Date(date2);
+  const d2 = new Date(date1);
+   const diff = d1-d2;
+  const ans = Math.floor(diff/(1000 * 60 * 60 * 24 ));
+	return ans;
+	
 };
 
 // Do not change the code below.
